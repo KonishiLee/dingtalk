3ec963a530d47e6d77e55db0214b256d482aa63d
Junv <wahdy4@gmail.com>
2016-05-19 11:50:18 +0800

    Add method to get department info

--- lib/dingtalk/api/department.rb
+++ lib/dingtalk/api/department.rb
@@ -5,10 +5,14 @@ module Dingtalk
         http_get("list?access_token=#{access_token}")
       end
 
+      def info dep_id
+        http_get("get?access_token=#{access_token}&id=#{dep_id}")
+      end
+
       private
-        def base_url
-          'department'
-        end
+      def base_url
+        'department'
+      end
     end
   end
 end
