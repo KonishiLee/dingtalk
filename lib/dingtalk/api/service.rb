b4607b9afb9e0983b3dfce68ee7c531d512e583f
Junv <wahdy4@gmail.com>
2016-05-30 16:48:41 +0800

    Add API to get auth info

---
+++ lib/dingtalk/api/service.rb
@@ -0,0 +1,25 @@
+module Dingtalk
+	module Api
+		class Service < Base
+
+			def auth_info
+				params = {
+					suite_key: Dingtalk.suite_key,
+					auth_corpid: corp_id,
+					permanent_code: permanent_code
+				}
+				http_post("get_auth_info?suite_access_token#{suite.suite_access_token}", params)
+			end
+
+			def suite
+				Api::Suite.new
+			end
+
+			private
+			def base_url
+				'service'
+			end
+		end
+	end
+end
+
