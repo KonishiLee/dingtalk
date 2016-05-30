df4a87038a1897dbd8a2832da424b2a35912bc19
Junv <wahdy4@gmail.com>
2016-05-30 17:16:32 +0800

    fix a typo

--- lib/dingtalk/api/service.rb
+++ lib/dingtalk/api/service.rb
@@ -8,7 +8,7 @@ module Dingtalk
 					auth_corpid: corp_id,
 					permanent_code: permanent_code
 				}
-				http_post("get_auth_info?suite_access_token#{suite.suite_access_token}", params)
+				http_post("get_auth_info?suite_access_token=#{suite.suite_access_token}", params)
 			end
 
 			def suite
